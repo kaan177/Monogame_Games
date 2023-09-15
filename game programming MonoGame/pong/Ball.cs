d3db6b3e098ba2df0b1c446f7a907b36d4cbc981
Dash-Karel <144909369+Dash-Karel@users.noreply.github.com>
2023-09-15 23:54:51 +0200

    Random start direction implemented

--- game programming MonoGame/pong/Ball.cs
+++ game programming MonoGame/pong/Ball.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 
@@ -7,21 +8,28 @@ namespace pong
     class Ball
     {
         Texture2D ball;
-        Vector2 position, speed, origin;
-        float speedMultiplier;
+        Vector2 position, startPosition, velocity, origin;
+        float speedMultiplier, startSpeed;
         Paddle paddle1, paddle2;
-        double lastBounce;      
+        double lastBounce;
+        System.Random random;
+
         public void Update(GameTime _gameTime)
         {
-            position += speed;
+            position += velocity;
             CheckCollision(_gameTime);
+        }
+        public void Draw(SpriteBatch _spriteBatch)
+        {
+            _spriteBatch.Draw(ball, position - origin, Microsoft.Xna.Framework.Color.White);
         }
         void CheckCollision(GameTime _gameTime)
         {
+            //check for collison with top and bottom of the screen
             if (position.Y < 0 + origin.Y || position.Y > Pong.screenSize.Y - origin.Y)
             {
-                speed.Y = -speed.Y;
-                speed *= speedMultiplier;
+                velocity.Y = -velocity.Y;
+                velocity *= speedMultiplier;
             }
             //check if bounce hasn't occured in the last 200 miliseconds
             if (_gameTime.TotalGameTime.TotalMilliseconds > lastBounce)
@@ -32,13 +40,13 @@ namespace pong
                     //check whether ball hits from top OR bottom
                     if (position.Y + origin.Y >= paddle1.Position.Y + paddle1.Height || position.Y - origin.Y <= paddle1.Position.Y && position.X > paddle1.Position.X && position.X < paddle1.Position.X + paddle1.Width)
                     {
-                        speed.Y = -speed.Y;
-                        speed *= speedMultiplier;
+                        velocity.Y = -velocity.Y;
+                        velocity *= speedMultiplier;
                     }
                     else
                     {
-                        speed.X = -speed.X;
-                        speed *= speedMultiplier;
+                        velocity.X = -velocity.X;
+                        velocity *= speedMultiplier;
                     }
                     lastBounce = _gameTime.TotalGameTime.TotalMilliseconds + 200;
                 }
@@ -48,31 +56,38 @@ namespace pong
                     //check whether ball hits from top OR bottom
                     if (position.Y + origin.Y >= paddle2.Position.Y + paddle2.Height || position.Y - origin.Y <= paddle2.Position.Y && position.X > paddle2.Position.X && position.X < paddle2.Position.X + paddle2.Width)
                     {
-                        speed.Y = -speed.Y;
-                        speed *= speedMultiplier;
+                        velocity.Y = -velocity.Y;
+                        velocity *= speedMultiplier;
                     }
                     else
                     {
-                        speed.X = -speed.X;
-                        speed *= speedMultiplier;
+                        velocity.X = -velocity.X;
+                        velocity *= speedMultiplier;
                     }
                     lastBounce = _gameTime.TotalGameTime.TotalMilliseconds + 200;
                 }
             } 
         }
-        public void Draw(SpriteBatch _spriteBatch)
+        void Reset()
         {
-            _spriteBatch.Draw(ball, position - origin, Color.White);
+            position = startPosition;
+            velocity = new Vector2(random.Next(-10, 10), random.Next(-10, 10));
+            velocity.Normalize();
+            velocity *= startSpeed;
+            
         }
+
         public Ball(Vector2 _startPosition, ContentManager _Content, Paddle _paddle1, Paddle _paddle2)
         {
             ball = _Content.Load<Texture2D>("ball");
             origin = new Vector2(ball.Width, ball.Height) / 2;
-            position = _startPosition;
-            speed = new Vector2(-3,3);
-            speedMultiplier = 1.0f;
+            startPosition = _startPosition;
+            speedMultiplier = 1.05f;
+            startSpeed = 3f;
             paddle1 = _paddle1;
             paddle2 = _paddle2;
+            random = new System.Random();
+            Reset();
         }
 
 
