f5a8edff96b9bfd96430e9dd3218c27f3691a19c
kaan177 <66557991+kaan177@users.noreply.github.com>
2023-09-15 16:06:31 +0200

    added player id

--- game programming MonoGame/pong/Paddle.cs
+++ game programming MonoGame/pong/Paddle.cs
@@ -5,24 +5,29 @@ using System.Diagnostics;
 
 namespace pong
 {
-     class Paddle
+     class Player
     {   
         //Class variables.
         Vector2 position;
         Texture2D sprite;
         Keys keyup, keydown;
         KeyboardState keyboard;
+        int playerId;
+        int maxHealth = 3;
+        int health;
         int speed = 5;
 
 
         
-        public Paddle(Vector2 _startposition, Texture2D _sprite, Keys _keyup, Keys _keydown)
+        public Player(Vector2 _startPosition, Texture2D _sprite, Keys _keyUp, Keys _keyDown, int _playerId)
         { 
             //Constructing variables.
-            position = _startposition;
+            position = _startPosition;
             sprite = _sprite;
-            keyup = _keyup;
-            keydown = _keydown;
+            keyup = _keyUp;
+            keydown = _keyDown;
+            playerId = _playerId;
+            health = maxHealth;
             
         }
         public void Update()
@@ -60,6 +65,16 @@ namespace pong
             if (position.Y > Pong.screenSize.Y - sprite.Height)
             {
                 position.Y = Pong.screenSize.Y - sprite.Height;
+            }
+        }
+
+        public void TakeDamage(int damage)
+        {
+            health -= damage;
+            if (health <= 0)
+            {
+                //Code to handle dying
+
             }
         }
     }
