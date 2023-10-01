1cfba96dcdf4806a887e848924a3c1bc786c83be
Dash-Karel <144909369+Dash-Karel@users.noreply.github.com>
2023-10-01 22:13:00 +0200

    added comments to paddle and movingGameObject

--- game programming MonoGame/pong/MovingGameObject.cs
+++ game programming MonoGame/pong/MovingGameObject.cs
@@ -5,15 +5,21 @@ namespace pong
 {
     class MovingGameObject : GameObject
     {
+        //class that can't be instantiated, it is a framework to build upon for game objects that need to be able to move. It inherits from GameObject and is the parent class of Ball and Player
+
         protected Vector2 lastPosition, velocity;
         protected MovingGameObject(ContentManager _content, string _textureString, Vector2 _startPosition, Pong _pong) : base(_content, _textureString, _startPosition, _pong)
         {
         }
+
+        //updates the position by adding the velocity per second and saves the position of the last frame
         public override void Update(GameTime gameTime)
         {
             lastPosition = position;
             position += velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
         }
+
+        //resets the position(base) last position and sets the velocity to zero
         public override void Reset()
         {
             base.Reset();
--- game programming MonoGame/pong/Paddle.cs
+++ game programming MonoGame/pong/Paddle.cs
@@ -8,7 +8,9 @@ namespace pong
 {
     internal class Player : MovingGameObject
     {
-        //Class variables.
+        /* The class player is responsible for the movement and drawing of the players, as well as keeping track of health and drawing health.
+        The class inherits all functionality of MovingGameObject and GameObject. The class also serves as a framework to Bot as it inherets from Player */
+
         Texture2D regularTexture ,shrunkTexture,redHeartTex, blueHeartTex, greenHeartTex, pinkHeartTex;
         Vector2 heartOrigin;
         SoundEffect damageSound;
@@ -56,15 +58,20 @@ namespace pong
                     startPosition.Y -= origin.Y;
                     break;
             }
+
+            //calls full reset to make sure all variables are in their initial state
             GameReset();
         }
 
         public override void Draw(SpriteBatch spriteBatch)
         {
+            //only draws if the player is active / alive
             if (isAlive)
             {
+                //draws the main texture
                 base.Draw(spriteBatch);
 
+                //draws at different positions based on the if the game is in 4 player mode or not
                 if (!pong.IsFourPlayers)
                 {
                     //drawing the health at the right position based on which player the health belongs to
@@ -72,6 +79,7 @@ namespace pong
                     {
                         for (int i = 0; i < health; i++)
                         {
+                            //draws from top left to the right
                             spriteBatch.Draw(redHeartTex, new Vector2(20 + i * 2f * heartOrigin.X * 2, 20), null, Color.White * 0.5f, 0f, Vector2.Zero, 2f, SpriteEffects.None, 0);
                         }
                     }
@@ -79,35 +87,41 @@ namespace pong
                     {
                         for (int i = 0; i < health; i++)
                         {
+                            //draws from top right to left
                             spriteBatch.Draw(redHeartTex, new Vector2(Pong.screenSize.X - (i + 1) * 2f * heartOrigin.X * 2 - 20, 20), null, Color.White * 0.5f, 0f, Vector2.Zero, 2f, SpriteEffects.None, 0);
                         }
                     }
                 }
                 else
                 {
-                    switch(playerId)
+                    //drawing the health at the right position based on which player the health belongs to
+                    switch (playerId)
                     {
                         case 0:
                             for (int i = 0; i < health; i++)
                             {
+                                //draws above and to the left of the centre the screen
                                 spriteBatch.Draw(redHeartTex, new Vector2(Pong.screenSize.X / 2 - heartOrigin.X * 12.5f + i * 4 * heartOrigin.X, Pong.screenSize.Y / 2 - heartOrigin.Y * 2.5f), null, Color.White * 0.3f, 0f, heartOrigin, 2f, SpriteEffects.None, 0);
                             }
                             break;
                         case 1:
                             for (int i = 0; i < health; i++)
                             {
+                                //draws above and to the right of the centre the screen
                                 spriteBatch.Draw(blueHeartTex, new Vector2(Pong.screenSize.X / 2 + heartOrigin.X * 0.5f + i * 4 * heartOrigin.X, Pong.screenSize.Y / 2 + heartOrigin.Y * 2.5f), null, Color.White * 0.3f, 0f, heartOrigin, 2f, SpriteEffects.None, 0);
                             }
                             break;
                         case 2:
                             for (int i = 0; i < health; i++)
                             {
+                                //draws below and to the left of the centre the screen
                                 spriteBatch.Draw(greenHeartTex, new Vector2(Pong.screenSize.X / 2 + heartOrigin.X * 0.5f + i * 4 * heartOrigin.X, Pong.screenSize.Y / 2 - heartOrigin.Y * 2.5f), null, Color.White * 0.3f, 0f, heartOrigin, 2f, SpriteEffects.None, 0);
                             }
                             break;
                         case 3:
                             for (int i = 0; i < health; i++)
                             {
+                                //draws below and to the right of the centre the screen
                                 spriteBatch.Draw(pinkHeartTex, new Vector2(Pong.screenSize.X / 2 - heartOrigin.X * 12.5f + i * 4 * heartOrigin.X, Pong.screenSize.Y / 2 + heartOrigin.Y * 2.5f), null, Color.White * 0.3f, 0f, heartOrigin, 2f, SpriteEffects.None, 0);
                             }
                             break;
@@ -115,16 +129,20 @@ namespace pong
                 }
             }
         }
+
         public override void Update(GameTime gameTime)
         {
+            //only draws if the player is active or alive
             if (isAlive)
             {
+                //HandleInput and move the player(base) after velocity is updated by HandleInput
                 HandleInput();
                 base.Update(gameTime);
 
                 // Resolving collision with the screen and other paddles.
                 if (isVertical)
                 {
+                    //limit the movement between bottom and top of screen. With an offset of the width of the player to make sure it doesnt collide with players at the top of bottom of it 
                     if (position.Y < origin.Y + 2 * origin.X)
                     {
                         position.Y = origin.Y + 2 * origin.X;
@@ -138,6 +156,7 @@ namespace pong
                 }
                 else
                 {
+                    //limit the movement between left and right of screen. With an offset of the height of the player to make sure it doesnt collide with players at the left of right of it 
                     if (position.X < origin.X + 2 * origin.Y)
                     {
                         position.X = origin.X + 2 * origin.Y;
@@ -152,9 +171,9 @@ namespace pong
             }
         }
 
+        // Taking player input and setting velocity accordingly.
         protected virtual void HandleInput()
         {
-            // Taking player input and setting velocity accordingly.
             keyboard = Keyboard.GetState();
             if (keyboard.IsKeyDown(keyup) && !keyboard.IsKeyDown(keydown))
             {
@@ -173,6 +192,8 @@ namespace pong
             else
                 velocity = Vector2.Zero;
         }
+
+        //TakeDamage is called by ball when the health of a player should decrement
         public void TakeDamage(int damage)
         {
             health -= damage;
@@ -185,24 +206,36 @@ namespace pong
                 pong.CheckGameOver();
             }
         }
+
+        //resets all variables that could change during the game , called at the start of the program or when the game is restarted via the main menu or game over screen
         public void GameReset()
         {
             isAlive = true;
             health = maxHealth;
             Reset();
         }
+
+        //Make the players smaller 
         public void Shrink()
         {
             texture = shrunkTexture;
             origin = new Vector2(Width, Height) / 2f;
+
+            //makes sure relevant variables are recalculated when the texture and origin changes
             ReCalculateVariables();
         }
+
+        //Resets players to their original size
         public void UnShrink()
         {
             texture = regularTexture;
             origin = new Vector2(Width, Height) / 2f;
+
+            //makes sure relevant variables are recalculated when the texture and origin changes
             ReCalculateVariables();
         }
+
+        //Increments health if not already at full health, called by PowerUps when the health power up is hit
         public void HealthUp()
         {
             if (!(health == maxHealth))
@@ -210,16 +243,22 @@ namespace pong
                 health++;
             }
         }
+
+        //inverts controls by rebinding the up and down key to the opposite values that were provided in the constructor. Called by PowerUps when the reverse controls power up is hit
         public void ReverseControls()
         {
             keyup = keydownStore;
             keydown = keyupStore;
         }
+
+        //resets the controls to the default by rebinding the up and down key to the default values that were provided in the constructor. Called by PowerUps when the reverse controls power up ends
         public void ResetControls()
         {
             keyup = keyupStore;
             keydown = keydownStore;
         }
+
+        //recalculates variables to make sure everything works correctly after a texture change
         public virtual void ReCalculateVariables()
         {
             switch (playerId)
@@ -243,38 +282,47 @@ namespace pong
             }
         }
 
+        //used to check if the instance of Player is active by many different classes, so its needs to be accesible
         public bool IsAlive
         {
             get { return isAlive; }
             set { isAlive = value; }
         }
+
+        //used to check the orientation of the paddle, mainly for collision detection by Ball
         public bool IsVertical
         {
             get { return isVertical; }
         }
+
+        //needs to be accesed by many classes for checking which player (left 0 or 4, right 1 or 5, top 2 or 6,  bottom 3 or 7) the instance is
         public int PlayerId
         {
             get { return playerId; }
         }
+
+        //needs to be accesible by ball for collision detection
         public int Width
         {
             get { return texture.Width; }
         }
+
+        //needs to be accesible by ball for collision detection
         public int Height
         {
             get { return texture.Height; }
         }
+
+        //returns the absolute position of the player(the top left point), needs to be accesible by ball for collision detection
         public Vector2 OriginAdjustedPosition
         {
             get { return position - origin; }
         }
+
+        //returns the absolute position of the player(the top left point) from the last frame, needs to be accesible by ball for collision detection
         public Vector2 OriginAdjustedLastPosition
         {
             get { return lastPosition - origin; }
-        }
-        public Vector2 Velocity
-        {
-            get { return velocity; }
         }
     }
 }
