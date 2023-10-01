8d6e202219f36cf3b72d138245c284c95c408ff3
kaan177 <66557991+kaan177@users.noreply.github.com>
2023-10-01 22:31:49 +0200

    Button comments

--- game programming MonoGame/pong/Buttons.cs
+++ game programming MonoGame/pong/Buttons.cs
@@ -6,6 +6,8 @@ namespace pong
 {
     internal class Button
     {   
+        /*Buttons are used in the Main Menu state and the Game Over state. The buttons are toggle butons meaning they can either be on or of. 
+            This is represented by the public isPressed boolean. */
         MouseState mouse, previousMouse;
         Vector2 topLeftPosition, textPosition, size;
         string buttonText;
@@ -29,41 +31,46 @@ namespace pong
 
             topLeftPosition = _topLeftPoint;
             mouseDetector = new Rectangle(_topLeftPoint.ToPoint(), _size.ToPoint());
-            textPosition = mouseDetector.Center.ToVector2() - standardFont.MeasureString(buttonText) / 2; //Centering the text in the rectangle
+            //Centering the text in the rectangle.
+            textPosition = mouseDetector.Center.ToVector2() - standardFont.MeasureString(buttonText) / 2; 
             isPressed = false;
             colorHoverdFactor = 0.5f;
         }
         public void Update()
         {
-            //Detecting if the mouse is with in the button and if its pressed
+            //Detecting if the mouse is with in the button and if its pressed.
             previousMouse = mouse;
             mouse = Mouse.GetState();
             if (mouseDetector.Contains(mouse.Position))
-            {
+            {// if the button is hovered by the mouse the button color is the full brightnes.
                 colorHoverdFactor = 1.0f;
+                //Check if the left button is being clicked and making sure that the button does not switch when the left button is held.
                 if (mouse.LeftButton == ButtonState.Pressed && previousMouse.LeftButton != ButtonState.Pressed)
                     isPressed = !isPressed;
 
             }
             else
-            {
+            {//when the button is not hovered the color brightnes is reduced.
                 colorHoverdFactor = 0.5f;
             }
             
         }
         public void Draw(SpriteBatch _spriteBatch)
         {   
-            //Drawing the button texture and the text
+            //Switching the color depending on if its pressed or not
             if (isPressed) 
-            { 
+            {   
                 _spriteBatch.Draw(buttonTexture, topLeftPosition, isPressedCol * colorHoverdFactor);
             }
             else
             {
                 _spriteBatch.Draw(buttonTexture, topLeftPosition, isNotPressedCol * colorHoverdFactor);
             }
+            //Drawing the text with in the button.
             _spriteBatch.DrawString(standardFont, buttonText, textPosition, Color.Black);
         }
+
+        //This method updates the position for when the screensize is changed.
         public void UpdatePosition(Vector2 position)
         {
             topLeftPosition = position;
